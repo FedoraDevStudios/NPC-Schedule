df0cc4210b74ba7241afe01c702f8154e6353042
FedoraGameDev <fedora.game.dev@gmail.com>
2022-02-13 00:23:23 -0800

    Added operation type to ManyTaskFilter

--- Implementations/ManyTaskFilter.cs
+++ Implementations/ManyTaskFilter.cs
@@ -3,11 +3,31 @@ using UnityEngine;
 
 namespace FedoraDev.NPCSchedule.Implementations
 {
+	public enum Operation
+	{
+		And,
+		Or
+	}
+
 	public class ManyTaskFilter : ITaskFilter
 	{
+		[SerializeField] Operation _filterOperation = Operation.And;
 		[SerializeField] ITaskFilter[] _taskFilters = new ITaskFilter[0];
 
-		public bool IsValid(IContext context) => _taskFilters.All(taskFilter => taskFilter.IsValid(context));
 		public ITaskFilter Produce() => new ManyTaskFilter();
+		public bool IsValid(IContext context)
+		{
+			switch (_filterOperation)
+			{
+				case Operation.And:
+					return _taskFilters.All(taskFilter => taskFilter.IsValid(context));
+
+				case Operation.Or:
+					return _taskFilters.Any(taskFilter => taskFilter.IsValid(context));
+			}
+
+			Debug.LogError("If you are seeing this, the ManyTaskFilter is operating with an unknown Operation type.");
+			return false;
+		}
 	}
 }
